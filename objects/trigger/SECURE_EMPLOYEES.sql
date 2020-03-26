8a5b34c4e86923df93e6f3f034b0eecbae658871
SteveZ <szandany@datical.com>
2020-03-26 15:09:26 -0500

    Repo updated

---
+++ objects/trigger/SECURE_EMPLOYEES.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE TRIGGER "SECURE_EMPLOYEES" 
+  BEFORE INSERT OR UPDATE OR DELETE ON employees
+BEGIN
+  secure_dml;
+END secure_employees;
--- sql/sqlsample.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
--- changeset id:foo author:bar
-CREATE TABLE "TABLE1119"
-   (	"COLUMN1" VARCHAR2(20)
-   )
-
--- rollback
--- drop table "TABLE1119"
